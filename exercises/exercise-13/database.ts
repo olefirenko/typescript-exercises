d2e4004fccf68d2596a47411f77689867346662c
olefirenko <dmitriy.olefirenko@gmail.com>
2020-06-12 21:02:21 +0800

    Add typings

--- exercises/exercise-13/database.ts
+++ exercises/exercise-13/database.ts
@@ -1,13 +1,30 @@
+type Filter = {
+    $eq: string | number,
+    $gt: string | number,
+    $lt: string | number,
+    $in: number[],
+    $text: string,
+}
+
+type FieldFilter<T> = {[P in keyof T]: Partial<Filter>}
+
+type BooleanSearch<T> = {
+    $and: Partial<FieldFilter<T>>[]
+    $or: Partial<FieldFilter<T>>[]
+}
+
+type TextSearch = {$text: string}
+
 export class Database<T> {
     protected filename: string;
-    protected fullTextSearchFieldNames: unknown[];
+    protected fullTextSearchFieldNames: (keyof T)[];
 
-    constructor(filename: string, fullTextSearchFieldNames) {
+    constructor(filename: string, fullTextSearchFieldNames: (keyof T)[]) {
         this.filename = filename;
         this.fullTextSearchFieldNames = fullTextSearchFieldNames;
     }
 
-    async find(query): Promise<T[]> {
+    async find(query: Partial<BooleanSearch<T>> | TextSearch | Partial<FieldFilter<T>>): Promise<T[]> {
         return [];
     }
 }
